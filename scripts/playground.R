c25041085eacd32beabcd096378a12d0084de3a8
diegodiaz <64859991+diodz@users.noreply.github.com>
2020-07-18 03:07:57 -0500

    Worked on R code, building Western Offshoots

--- scripts/playground.R
+++ scripts/playground.R
@@ -1,29 +1,30 @@
 library(readxl)
-library(plyr)
 library(dplyr)
 
-mpd2018 <- read_excel("data/mpd2018.xlsx",
-sheet = "cgdppc")
+#The purpose of this section is to produce two files from the Maddison 2018
+#database to perform synthetic controls in MATLAB
+
+mpd2018 <- read_excel("../data/mpd2018.xlsx", sheet = "cgdppc")
 
 #We delete the row of country codes
 mpd2018 <- mpd2018[-c(1),]
 
 #Renaming first column
-mpd2018 <- rename(mpd2018, c("cgdppc"="year"))
+mpd2018 <- rename(mpd2018, c("year"="cgdppc"))
 
-chile_and_controls <- c('year', 'Chile', 'Australia', 'Canada', 'Switzerland',
-                        'Denmark', 'Finland', 'Norway', 'New Zealand', 'Sweden',
-                        'Portugal')
+chile_and_western_offshoots <- c('year', 'Chile', 'Australia', 'New Zealand',
+                                 'Canada', 'United States')
+
+#Keep the years that we need for synthetic controls, 1900 to 1960
+mpd2018$year <- as.numeric(mpd2018$year)
+cgdppc <- mpd2018[chile_and_western_offshoots][mpd2018$year >= 1810,]
+cgdppc <- mutate_all(df, function(x) as.numeric(as.character(x)))
+
+#Read population data
+mpd2018 <- read_excel("../data/mpd2018.xlsx", sheet = "pop")
+mpd2018 <- mpd2018[-c(1),]
+mpd2018$year <- as.numeric(mpd2018$year)
+mpd2018 <- rename(mpd2018, c("year"="pop"))
+pop <- mpd2018[chile_and_western_offshoots][mpd2018$year >= 1810,]
 
 
-df <- mpd2018[chile_and_controls][mpd2018$year >= 1900 & mpd2018$year <= 1960,]
-
-df <- mutate_all(df, function(x) as.numeric(as.character(x)))
-
-write.csv(df, "../data/gdppc.csv", row.names=FALSE)
-
-write.csv(colnames(df)[2:11], "../data/countries.csv", row.names=FALSE)
-
-#europe12 <- c()
-
-#df[c("A","B","E")]
