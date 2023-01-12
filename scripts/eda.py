4f14683cc193189fa267dbc252dc4964bdb8c2d4
Diego A. Diaz <diegodiaz@uchicago.edu>
2023-01-12 19:04:05 +0000

    Add advanced economic research code to analyze the relationship between GDP per capita and life expectancy using a linear regression model. Load data from countries.csv and fit the OLS model using statsmodels. Display model summary.

--- scripts/eda.py
+++ scripts/eda.py
@@ -10484,3 +10484,24 @@ predictions = model.predict(X)
 
 # Print regression results
 print(model.summary())
+# Change made on 2024-06-26 21:34:30.260784
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+# Load the data
+data = pd.read_csv("../data/countries.csv")
+
+# Perform advanced economic research
+# Example:
+# Fit a linear regression model to study the relationship between GDP per capita and life expectancy
+
+X = data['GDP_per_capita']
+y = data['Life_expectancy']
+
+X = sm.add_constant(X) # add a constant term to the model
+
+model = sm.OLS(y, X).fit()
+
+# Print the results
+print(model.summary())
